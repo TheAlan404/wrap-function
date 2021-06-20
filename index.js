8656aac3cdda75a8a7bfb11b1a5bdfc71dda021c
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-06-20 22:14:59 +0300

    add emitter

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { EventEmitter } = require("events");
+
 function wrap(value, cb){
 	if(typeof value !== "function") {
 		throw new Error("Only functions are wrappable for now");
@@ -10,6 +12,23 @@ function wrap(value, cb){
 	value = function(){
 		_value(...arguments);
 		cb(...arguments);
+	};
+	
+	return wrap;
+};
+
+wrap.events = (emitter, cb) => {
+	if(!(emitter instanceof EventEmitter)) {
+		throw new Error("emitter must be an EventEmitter");
+	};
+	if(typeof cb !== "function") cb = (name, ...data) => {
+		console.log(`emitted event '${name}' ${data.length ? "with data"}`, ...data);
+	};
+	
+	emitter._emit = emitter.emit;
+	emitter.emit = (name, data) => {
+		cb(name, ...data);
+		emitter._emit(name, ...data);
 	};
 	
 	return wrap;
